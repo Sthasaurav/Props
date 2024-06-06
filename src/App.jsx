2141c1de3400532e1ac65f673dd96fd496347e77
Sthasaurav <92661343+Sthasaurav@users.noreply.github.com>
2024-06-06 20:15:52 +0545

    deep into props % evenListner

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,24 @@
+import React, { useState } from 'react';
+import jsx from './assets/jsx-ui.png';
+import CoreConcepts from './components/CoreConcepts.jsx';
+import TabButtons from './components/TabButtons.jsx';
+import { CORE_CONCEPTS } from './data';
 
-import jsx from './assets/jsx-ui.png'
-
-import { CORE_CONCEPTS } from './data'
-const random = ["Fundamental", "Crucial", "Absolute",]
+const random = ["Fundamental", "Crucial", "Absolute"];
 
 function getRandom(max) {
-
   return Math.floor(Math.random() * (max + 1));
-
-
-}
-
-function CoreConcepts(props) {
-  return (
-    <li>
-
-      <img src={props.image} alt={props.title} />
-      <h2>{props.title}</h2>
-      <p>{props.description}</p>
-    </li>
-  )
-
 }
 
 function App() {
-  const randomm = random[getRandom(2)]
+  const [selectedButton, setSelectedButton] = useState('');
+  const randomm = random[getRandom(2)];
+
+  function handleButtonClick(selectButton) {
+    setSelectedButton(selectButton);
+    console.log(selectButton);
+  }
+
   return (
     <div>
       <header>
@@ -34,38 +28,44 @@ function App() {
           {randomm} React concepts you will need for almost any app you are
           going to build!
         </p>
-        <main>
-          <section id="core-concepts" >
-<h2>Core Concept</h2>
-<ul>
-<CoreConcepts
+      </header>
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concept</h2>
+          <ul>
+            <CoreConcepts
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
-            
-            
             <CoreConcepts
               title={CORE_CONCEPTS[1].title}
               description={CORE_CONCEPTS[1].description}
               image={CORE_CONCEPTS[1].image}
-            />   <CoreConcepts
-            title={CORE_CONCEPTS[2].title}
-            description={CORE_CONCEPTS[2].description}
-            image={CORE_CONCEPTS[2].image}
-          />
-          <CoreConcepts {...CORE_CONCEPTS[3]}/>
-</ul>
-           
+            />
+            <CoreConcepts
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+              image={CORE_CONCEPTS[2].image}
+            />
+            <CoreConcepts {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
 
-
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButtons onSelect={() => handleButtonClick("Components")}>Components</TabButtons>
+            <TabButtons onSelect={() => handleButtonClick("Props")}>Props</TabButtons>
+            <TabButtons onSelect={() => handleButtonClick("JSX")}>JSX</TabButtons>
+            <TabButtons onSelect={() => handleButtonClick("State")}>State</TabButtons>
+          </menu>
+          <section>
+            {/* Show text that is written in the button */}
+            {selectedButton && <p>{selectedButton}</p>}
           </section>
-
-        </main>
-
-
-
-      </header>
+        </section>
+      </main>
       <main>
         <h2>Time to get started!</h2>
       </main>
---
+++ src/components/CoreConcepts.jsx
@@ -0,0 +1,15 @@
+
+
+export default function CoreConcepts(props) {
+        return (
+          <li>
+      
+            <img src={props.image} alt={props.title} />
+            <h2>{props.title}</h2>
+            <p>{props.description}</p>
+          </li>
+        )
+      
+      }
+
+      
